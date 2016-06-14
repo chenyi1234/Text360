d675eac9f038050419422040d3a731d1834179e9
chenyi <490901377@qq.com>
2016-06-14 08:36:10 +0800

    Revert "Revert "a""
    This reverts commit 7a7570ca7c3716eb191680145dd94ae41b76bdd9.

---
+++ Text360/PanoramaViewController.h
@@ -0,0 +1,13 @@
+//
+//  PanoramaViewController.h
+//  Text360
+//
+//  Created by Apple on 16/6/13.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface PanoramaViewController : GLKViewController
+
+@end
