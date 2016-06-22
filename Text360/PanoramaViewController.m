e2681385b6f13b5d7c6977f1936baef005dffc6d
chenyi <490901377@qq.com>
2016-06-22 11:40:38 +0800

    1
    1

--- Text360/PanoramaViewController.m
+++ Text360/PanoramaViewController.m
@@ -8,32 +8,69 @@
 
 #import "PanoramaViewController.h"
 #import "PanoramaView.h"
+#define HEIGHT [UIScreen mainScreen].bounds.size.height
+#define WIDTH  [UIScreen mainScreen].bounds.size.width
 @interface PanoramaViewController (){
     PanoramaView *panoramaView;
+    
 }
-
+@property (nonatomic ,assign) BOOL isFollow;
 @end
 
 @implementation PanoramaViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.isFollow = NO;
     panoramaView = [[PanoramaView alloc] init];
     // 尺寸固定的图片
-    [panoramaView setImageWithName:@"全景图.jpg"];
+    [panoramaView setImageWithName:@"全景图1.jpg"];
     // 是跟随设备 重力感应与否
-    [panoramaView setOrientToDevice:NO];
+    [panoramaView setOrientToDevice:self.isFollow];
     [panoramaView setTouchToPan:YES];
     [panoramaView setPinchToZoom:YES];
     // 显示 线
-    [panoramaView setShowTouches:YES];
+   // [panoramaView setShowTouches:YES];
     
-  
+    UIButton * btn = [[UIButton alloc]initWithFrame:CGRectMake(50, HEIGHT - 80,100, 40)];
+    [btn setTitle:@"跟随模式" forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor grayColor];
+    [btn addTarget:self action:@selector(clickToChooseType:) forControlEvents:UIControlEventTouchUpInside];
     
-    [self setView:panoramaView];
+     [self setView:panoramaView];
+    for (NSInteger i = 0 ; i < 9 ; i++) {
+        UIButton * btn = [[UIButton alloc]initWithFrame:CGRectMake(WIDTH -100, 40*i+40, 100, 30)];
+        [btn setTitle:[NSString stringWithFormat:@"%ld",(i+1)] forState:UIControlStateNormal];
+        btn.backgroundColor = [UIColor blackColor];
+        btn.tag = (i+1);
+        [btn addTarget:self action:@selector(clickToChangeImage:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:btn];
+        
+    }
+    
+   [self.view insertSubview:btn atIndex:0];
 
 }
+- (void)clickToChangeImage:(UIButton *)btn{
 
+   
+           [panoramaView setImageWithName:[NSString stringWithFormat:@"全景图%ld.jpg",btn.tag]];
+  
+
+    
+    NSLog(@"-%@-",[NSString stringWithFormat:@"全景图%ld.jpg",btn.tag]);
+   // [self.view reloadInputViews];
+
+}
+- (void)clickToChooseType:(UIButton *)btn{
+      self.isFollow = !self.isFollow;
+    [btn setTitle:[NSString stringWithFormat:@"%@",self.isFollow?@"手动模式":@"跟随模式"] forState:UIControlStateNormal];
+  
+    
+    [panoramaView setOrientToDevice:self.isFollow];
+
+
+}
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect{
     [panoramaView draw];
 
